4b2a48444d4775f98f3f43ba994ded19c52c24a3
udsadaptteam <s.goswami@adapt-india.com>
2026-01-15 18:45:09 +0530

    1

--- js/commen.js
+++ js/commen.js
@@ -1,4 +1,38 @@
 
+
+
+
+const entrCode = document.querySelector(".enter_code_bx"); 
+const password_area = document.querySelector(".password_area");
+
+
+const emailId = document.getElementById("youEmail");
+const validate_Code = document.getElementById("validate_code");
+const entcod = document.getElementById("enterCode");
+
+
+function verify_cod(itm){
+
+  if(itm.id === 'send_verfication_code'){
+    itm.style.display = 'none';
+    emailId.setAttribute('disabled', true);
+    validate_Code.style.display = 'block';
+    entrCode.style.display ="block";
+  }
+
+   // VALIDATE CODE
+
+ if(itm.id === 'validate_code'){
+   itm.style.display = 'none';
+   entcod.setAttribute('disabled', true);
+   password_area.style.display = 'block';
+   document.getElementById("nextStp").style.display = 'block';
+ } 
+ 
+};
+
+
+
 /**** form step code *****/
 
 const steps = document.querySelectorAll(".step");
@@ -59,3 +93,24 @@ function signOut() {
   document.querySelector('.login_first').classList.add('active');
 
 }
+
+
+
+/****show and hide password***/ 
+
+const eyeEvent = document.querySelectorAll('.eye_event');
+
+  eyeEvent.forEach(function(itm){
+    itm.onclick = function(){
+      let inpt  = itm.previousElementSibling;
+          if(inpt.type === 'password'){
+            itm.src = "img/eye-open.png";
+            inpt.type = "text";
+          }
+          else{
+            itm.src = "img/eye-close.png";
+            inpt.type = "password";
+          }
+    }
+  })
+
